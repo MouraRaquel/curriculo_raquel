053b6495fa46c3b34a721a9b97628a11aaa4551e
MouraRaquel <raquelaguiar@id.uff.br>
2022-06-02 07:30:44 -0300

    About

--- curriculo-raquel/src/app/app.module.ts
+++ curriculo-raquel/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
